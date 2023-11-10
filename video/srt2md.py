ada0270ea0a6ce00fb70d1d66bf93628b9cf95d6
Benature <wbenature@163.com>
2023-11-10 23:54:32 +0800

    convert to html

--- video/srt2md.py
+++ video/srt2md.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import sys
-# import subprocess
+import subprocess
 
 
 # Get directory path from command line argument
@@ -23,14 +23,20 @@ for srt_file in srt_files:
             subtitle = lines[i+2].strip()
             # print(start_time, subtitle)
             t = start_time.split(',')[0]
-            md_content += f"{subtitle} [🔗](kmtrigger://macro=1E194478-AABF-46EA-A53B-872E0554DCC2&value={t}) \n"
+            t_short = t[3:] if t.startswith('00:') else t
+            md_content += f"""[({t_short})](kmtrigger://macro=1E194478-AABF-46EA-A53B-872E0554DCC2&value={t_short}) {subtitle}  \n"""
         
     md_file = (srt_file.parent / "md" / srt_file.stem).with_suffix('.md')
     with open(md_file, 'w') as md_f:
         md_f.write(md_content)
 
 
-    # # Convert all .md files in the directory to .pdf files using pandoc
-    # pdf_file = (md_file.parent / md_file.stem).with_suffix('.html')
-    # subprocess.run(['pandoc', str(md_file), '-o', str(pdf_file)])
+    # Convert all .md files in the directory to .pdf files using pandoc
+    # pdf_file = (md_file.parent / md_file.stem).with_suffix('.pdf')
+    # subprocess.run(['pandoc', '--pdf-engine=xelatex', f'--template=pandoc/template.tex', '-V', 'urlcolor=NavyBlue', str(md_file), '-o', str(pdf_file)])
+    html_dir = srt_file.parent / "html"
+    if not html_dir.exists():
+        html_dir.mkdir()
+    html_fn = (html_dir / srt_file.stem).with_suffix('.html')
+    subprocess.run(['pandoc', str(md_file), '-o', str(html_fn)])
     # break
